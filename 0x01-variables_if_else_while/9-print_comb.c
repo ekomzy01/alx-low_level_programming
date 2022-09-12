be4a6a4071dde4cb49456a2546e5492799d0d01e
ekomzy01 <johnekomobongisaiah@gmail.com>
2022-09-11 17:02:56 -0700

    task 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,12 +1,12 @@
-#include <stdio.h>
+#include<stdio.h>
 /**
- *  * main - Prints numbers between 0 to 9 with commas and spaces.
- *  * Return: Always 0 (Success)
+ * main - prints numbers between 0 to 9 with commas and spaces
+ * Return: always 0
  */
 int main(void)
 {
 	int i;
-	
+
 	for (i = 48; i < 58; i++)
 	{
 		putchar(i);
