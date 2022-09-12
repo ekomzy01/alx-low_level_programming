c15905e442020454ebf4f0cf2c8c2b4ac1e0d73a
ekomzy01 <johnekomobongisaiah@gmail.com>
2022-09-11 17:09:05 -0700

    commit

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,10 +1,8 @@
 #include <stdio.h>
 /**
  *  * main - Prints numbers between 00 to 89.
- *
- *    * Return: Always 0 (Success)
+ *  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
 	int i, e;
