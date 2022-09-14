81508ac8ea6b2d217e015eebf76a7fd7bbad4855
ekomzy01 <johnekomobongisaiah@gmail.com>
2022-09-13 19:35:00 -0700

    task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * add - adds two numbers together and returns the value
+ * @x: int data type
+ * @y: int data type
+ * Return: int
+ */
+int add(int x, int y)
+{
+	return (x * y);
+}
