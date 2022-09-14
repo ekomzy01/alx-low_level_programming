4df1d51aa5abc50e702e2ae47fa70a6101701c89
ekomzy01 <johnekomobongisaiah@gmail.com>
2022-09-13 19:18:43 -0700

    task 9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * times_table - prints the 9 times table
+ * @n: int data type
+ * Return: n
+ */
+void times_table(void)
+{
+	int n = 0, m;
+
+	while (n < 10)
+	{
+		m = 0;
+		while (m < 10)
+		{
+			_putchar(n * m);
+			return (n * m);
+			m++;
+		}
+		_putchar('\n');
+		n++;
+	}
+}
