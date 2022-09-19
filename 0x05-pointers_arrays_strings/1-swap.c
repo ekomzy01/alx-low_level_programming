71b9b780ddde6d48a49b1c6bdc374b5ea7357a5c
ekomzy01 <johnekomobongisaiah@gmail.com>
2022-09-19 15:12:49 -0700

    task 1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * swap_int - swaps the values of two in
+ * @a: memory location of type int
+ * @b: ponter
+ * Return: Always 0
+ */
+void swap_int(int *a, int *b)
+{
+	*a = b;
+	*b = a;
+}
