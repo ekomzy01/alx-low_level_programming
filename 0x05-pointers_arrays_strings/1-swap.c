e31999d2ed6da1d8c0c3cfd723bb486ac11c1958
ekomzy01 <johnekomobongisaiah@gmail.com>
2022-09-19 18:17:54 -0700

    task 1

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -3,7 +3,7 @@
  * swap_int - swaps the values of two in
  * @a: memory location of type int
  * @b: ponter
- * @c: int vairable
+ * @cp: int vairable
  * Return: Always 0
  */
 void swap_int(int *a, int *b)
