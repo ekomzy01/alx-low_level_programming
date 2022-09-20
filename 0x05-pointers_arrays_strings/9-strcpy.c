758433c269c07e8be44afa1bea4f1099e72e6a09
ekomzy01 <johnekomobongisaiah@gmail.com>
2022-09-19 18:03:32 -0700

    task 9 betty warning

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include "main.h"
 /**
- * *_strcpy - copies the string pointed to by src, including the terminating null byte (\0), to the buffer pointed to by dest.
+ * *_strcpy - copies the string pointed to by src to the buffer pointed to by dest.
  * @dest: char type string
  * @src: char type string
  * Return: Pointer to `dest`
