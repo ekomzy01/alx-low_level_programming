0ad28363f04d667a07902bc94eba83e378994951
ekomzy01 <johnekomobongisaiah@gmail.com>
2022-09-26 13:05:05 -0700

    task 0

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * *_memset - file memory with constant byte
+ * @s: char data type
+ * @n: long int
+ * @b: constant byte
+ * Return: s pointer to memory area
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i;
+
+	i = 0;
+	while (n > 0)
+	{
+		s[i] = b;
+		i++;
+		n--;
+	}
+	return s;
+}
